03c497356c4833b09f2f1120aefeacd034377fbf
Tony Isidro <tonyi@Tonys-MBP.fios-router.home>
2018-04-26 11:31:59 -0400

    Initial Files

---
+++ liri.js
@@ -0,0 +1,13 @@
+// to read and set any environment variables with the dotenv package:
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+//my-tweets
+
+//spotify-this-song
+
+//movie-this
+
+//do-what-it-says
